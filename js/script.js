e41fcad94647511481b8213090e092a0ee2c941f
Borch0215 <109316226+Borch0215@users.noreply.github.com>
2023-06-01 13:18:53 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".infoAbout", {
+    strings: ["", "Diseñador Web", "", "Programador", "", "Diseñador Gráfico", "", "Deportista", "", "Gamer"],
+    typeSpeed: 100,
+    BackSpeed: 200,
+    loop: true
+})
